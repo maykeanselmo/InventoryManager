53fdebdf016cd9391c490c1627d8f00cc4ca7953
Mayke <mayke000000000@gmail.com>
2024-08-10 22:44:10 -0300

    feat: entidade user e suas operações básicas

--- User.h
+++ User.h
@@ -11,7 +11,7 @@ typedef struct User {
     char paymentMethod[20];
     char cpf[12];
 } TUser;
-TUser* user(char *name, char *adress, char *paymentMethod, char *cpf);
+TUser * user(char *name, char *adress, char *paymentMethod, char *cpf);
 int sizeUser();
 int qtdUserInFile(FILE* file);
 void printUser(TUser *user);
