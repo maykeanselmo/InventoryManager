e60ea940dda5ded2093f4c58d98d1ee40c4ccbcc
Athus <athussilva27@gmail.com>
2024-09-12 14:04:29 -0300

    initial commit

--- src/Interface.h
+++ src/Interface.h
@@ -1,12 +1,16 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h"
+#include "file/FileUtils.h"
+#include "algorithms/classificacaoInterna.h"
+#include "algorithms/intercalacaoBasico.h"
 void menu();
-void printMenu();
 void genDisordedBase();
 void searchProd();
 void removeProd();
-void createUser();
 void createOrder();
 void listBase();
 void finishOrder();
---
+++ src/algorithms/selecaoporsubstituicao.h
@@ -0,0 +1,11 @@
+#ifndef SELECAOPORSUBSTITUICAO_H_INCLUDED
+#define SELECAOPORSUBSTITUICAO_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+
+int selecao_por_substituicao(FILE *out, int num_p);
+void printProductCodes(const char *fileName);
+
+
+
+#endif 
